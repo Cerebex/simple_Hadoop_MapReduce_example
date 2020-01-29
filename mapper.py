15abe31942db3f7f5884248f81d867fa736f9d6c
Cerebex <miller.sam.h@gmail.com>
2020-01-29 04:09:21 +0000

    remove stop words, punctuation and lower case

--- mapper.py
+++ mapper.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import sys
-
+import string
+from sklearn.feature_extraction import text
+stops = set(text.ENGLISH_STOP_WORDS)
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-
+    # remove leading and trailing whitespace, lower case letters, remove punctuation 
+    line = line.strip().lower().translate(None, string.punctuation)    
     # split the line into words; splits on any whitespace
     words = line.split()
-
-    # output tuples (word, 1) in tab-delimited format
+    # output tuples (word, 1) in tab-delimited format if is stopword from nltk 
+    # english_stop_words
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stops:
+            print '%s\t%s' % (word, "1")
